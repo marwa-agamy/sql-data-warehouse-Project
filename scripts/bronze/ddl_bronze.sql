47e58b0b1f93001d857ae6330b03d16c782f37d4
Marwa Agamy <marwaagamy146@gmail.com>
2026-02-05 04:32:07 +0200

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,53 @@
+--Create Tables for each source_crm file
+--Create cust_info Table
+CREATE TABLE bronze.crm_cust_info(
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
+cst_create_date DATE
+)
+--Create prd_info Table
+CREATE TABLE bronze.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost DECIMAL(10,2),
+prd_line NVARCHAR(50),
+prd_start_dt DATE,
+prd_end_dt DATE
+)
+--Create sales_details Table
+CREATE TABLE bronze.crm_sales_details(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt DATE,
+sls_ship_dt DATE,
+sls_due_dt DATE,
+sls_sales DECIMAL(10,2),
+sls_quantity INT,
+sls_price DECIMAL(10,2)
+)
+------------------------------------------
+--Create Tables for each source_erp file
+--Create LOC_A101 Table
+CREATE TABLE bronze.erp_loc_a101(
+cid NVARCHAR(50),
+cntry NVARCHAR(50)
+)
+--Create CUST_AZ12 Table
+CREATE TABLE bronze.erp_cust_az12(
+cid NVARCHAR(50),
+bdate DATE,
+gen NVARCHAR(50)
+)
+--Create PX_CAT_G1V2 Table
+CREATE TABLE bronze.erp_px_cat_g1v2(
+id NVARCHAR(50),
+cat NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+)
