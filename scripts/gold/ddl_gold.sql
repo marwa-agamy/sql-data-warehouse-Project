7f5f95f35d85085c13a19c8fef5ad7f8bffb94ee
Marwa Agamy <marwaagamy146@gmail.com>
2026-02-10 23:24:40 +0200

    Add gold.dim_customers, gold.dim_products, and gold.fact_sales views
    Create views for customers, products, and sales in the gold schema.

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,57 @@
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
+CREATE VIEW gold.dim_customers AS
+SELECT 
+ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,
+ci.cst_id AS customer_id,
+ci.cst_key  AS customer_number,
+ci.cst_firstname AS first_name,
+ci.cst_lastname AS last_name,
+ce.cntry AS country,
+ci.cst_marital_status AS marital_status ,
+CASE WHEN ci.cst_gndr !='unknown' THEN ci.cst_gndr
+     ELSE COALESCE(ca.gen,'unknown')
+END AS gender,
+ca.bdate AS birthdate,
+ci.cst_create_date AS create_date
+from silver.crm_cust_info ci LEFT JOIN silver.erp_cust_az12 ca ON  ci.cst_key=ca.cid
+LEFT JOIN silver.erp_loc_a101 ce ON ca.cid=ce.cid
+
+-- =============================================================================
+-- Create Dimension: gold.dim_products
+-- =============================================================================
+CREATE VIEW gold.dim_products AS
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY pr.prd_start_dt,pr.prd_id) AS product_key,
+	pr.prd_id AS product_id,
+	pr.prd_key AS product_number ,
+	pr.prd_nm AS product_name,
+	pr.cat_id AS category_id,
+	px.cat AS category,
+	px.subcat AS subcategory,
+	px.maintenance,
+	pr.prd_cost AS cost,
+	pr.prd_line AS product_line,
+	pr.prd_start_dt AS start_date
+FROM silver.crm_prd_info pr 
+LEFT JOIN silver.erp_px_cat_g1v2 px ON pr.cat_id = px.id
+where prd_end_dt IS NULL --Filter Out All Historical Data --Current Data Only
+
+-- =============================================================================
+-- Create Fact Table: gold.fact_sales
+-- =============================================================================
+CREATE VIEW gold.fact_sales AS 
+SELECT 
+	sd.sls_ord_num AS order_number ,
+	pr.product_key,
+	cr.customer_key,
+	sd.sls_order_dt AS order_date ,
+	sd.sls_ship_dt AS ship_date,
+	sd.sls_due_dt AS due_date ,
+	sd.sls_sales AS sales_amount ,
+	sd.sls_quantity AS quantity,
+	sd.sls_price AS price
+FROM silver.crm_sales_details sd LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number LEFT JOIN gold.dim_customers cr
+ON sd.sls_cust_id = cr.customer_id 
